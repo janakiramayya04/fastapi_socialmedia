e6ad3f8952c19243b991d965270a8921b8919df4
Janaki ramayya pasagada <143267493+janakiramayya04@users.noreply.github.com>
2025-07-29 10:48:57 +0530

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,7 @@ from app.models import Base
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option(
-    "sqlalchemy.url", "mysql+pymysql://root:rootpass@localhost:3306/socialmedia"
+    "sqlalchemy.url", "postgresql://postgres:J@N@KI004@db.vwojaiguftbspitgrros.supabase.co:5432/postgres"
 )
 
 # Interpret the config file for Python logging.
